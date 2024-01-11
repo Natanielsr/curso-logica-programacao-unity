34455eb1decf8c7299f06e0e7b090cbb65149169
Nataniel Soares Rodrigues <contatonatanielsoares@gmail.com>
2024-01-11 08:45:47 -0300

    Create aula5-variaveis-constantes.cs

---
+++ aula5-variaveis-constantes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int idade; // Declaração de uma variável inteira chamada "idade"
+        idade = 25; // Atribuição de um valor à variável
+        print(idade); //exibe o valor da variavel idade no console
+
+        const float pi = 3.14f; // Declaração de uma constante float chamada "pi"
+        print(pi); //exibe o valor da constante pi no console
+
+    }
+
+}
